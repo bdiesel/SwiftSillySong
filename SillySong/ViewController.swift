a1ffaad1a9481430a72100659dec63ee3255eeae
Brian Diesel <brian@designcondition.com>
2019-09-06 15:32:59 -0400

    Initial Commit

---
+++ SillySong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SillySong
+//
+//  Created by Brian Diesel on 9/6/19.
+//  Copyright © 2019 DesignCondition. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
